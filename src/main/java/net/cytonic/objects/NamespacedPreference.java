1cc8a333c46b60f3c1817e0e9eba74baad81d1fe
foxikle <tristantroop260@gmail.com>
2024-07-30 15:03:45 -0400

    Add nullability to NamespacedPreference

--- src/main/java/net/cytonic/objects/NamespacedPreference.java
+++ src/main/java/net/cytonic/objects/NamespacedPreference.java
@@ -1,6 +1,7 @@
 package net.cytonic.objects;
 
 import net.minestom.server.utils.NamespaceID;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * A record class holding a preference with a namespace
@@ -9,4 +10,13 @@ import net.minestom.server.utils.NamespaceID;
  * @param <T> the type of the preference
  */
 public record NamespacedPreference<T>(NamespaceID namespaceID, T value) {
+
+    public NamespacedPreference(NamespaceID namespaceID, Class<T> type, @Nullable T value) {
+        this(namespaceID, type.cast(value));
+    }
+
+    public NamespacedPreference(NamespaceID namespaceID, T value) {
+        this.namespaceID = namespaceID;
+        this.value = value;
+    }
 }
