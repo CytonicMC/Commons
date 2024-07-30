4864e5959114a5f3f6d858cd54aca0de677f4fc9
foxikle <tristantroop260@gmail.com>
2024-07-30 15:05:01 -0400

    Add javadocs

--- src/main/java/net/cytonic/objects/NamespacedPreference.java
+++ src/main/java/net/cytonic/objects/NamespacedPreference.java
@@ -11,10 +11,21 @@ import org.jetbrains.annotations.Nullable;
  */
 public record NamespacedPreference<T>(NamespaceID namespaceID, T value) {
 
+    /**
+     * Creates a new {@link NamespacedPreference}, with an optionally null value. The type must be specified manually
+     * @param namespaceID the namespace
+     * @param type the type of the preference
+     * @param value the default value, nullable
+     */
     public NamespacedPreference(NamespaceID namespaceID, Class<T> type, @Nullable T value) {
         this(namespaceID, type.cast(value));
     }
 
+    /**
+     * Creates a new {@link NamespacedPreference}
+     * @param namespaceID the namespace
+     * @param value the default value
+     */
     public NamespacedPreference(NamespaceID namespaceID, T value) {
         this.namespaceID = namespaceID;
         this.value = value;
