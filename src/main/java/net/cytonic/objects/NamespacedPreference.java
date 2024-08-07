b8fe8d929563dd7e20f9420ddb70ada4eedf70e9
foxikle <tristantroop260@gmail.com>
2024-08-07 14:18:07 -0400

    Enforce types

--- src/main/java/net/cytonic/objects/NamespacedPreference.java
+++ src/main/java/net/cytonic/objects/NamespacedPreference.java
@@ -9,7 +9,7 @@ import org.jetbrains.annotations.Nullable;
  * @param value The default value
  * @param <T> the type of the preference
  */
-public record NamespacedPreference<T>(NamespaceID namespaceID, T value) {
+public record NamespacedPreference<T>(TypedNamespace<T> namespaceID, T value) {
 
     /**
      * Creates a new {@link NamespacedPreference}, with an optionally null value. The type must be specified manually
@@ -17,7 +17,7 @@ public record NamespacedPreference<T>(NamespaceID namespaceID, T value) {
      * @param type the type of the preference
      * @param value the default value, nullable
      */
-    public NamespacedPreference(NamespaceID namespaceID, Class<T> type, @Nullable T value) {
+    public NamespacedPreference(TypedNamespace<T> namespaceID, Class<T> type, @Nullable T value) {
         this(namespaceID, type.cast(value));
     }
 
