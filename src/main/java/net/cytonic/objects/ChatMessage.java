36e25984e6692bc86aa06dee4a0eab0febba727f
foxikle <tristantroop260@gmail.com>
2024-08-11 10:23:37 -0400

    Reformat

--- src/main/java/net/cytonic/objects/ChatMessage.java
+++ src/main/java/net/cytonic/objects/ChatMessage.java
@@ -2,23 +2,22 @@ package net.cytonic.objects;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 import net.cytonic.enums.ChatChannel;
 import org.jetbrains.annotations.Nullable;
 
-import java.lang.reflect.Type;
 import java.util.List;
 import java.util.UUID;
 
 /**
  * A class that represents a message sent to the specified recipients.
- * @apiNote The {@code recipients} field may be null, indicating the message should be broadcast out on the channel, rather than sent only to the recipients.
- * @param recipients The intended recipients of the message, may be null. See note above.
- * @param channel The channel the message is sent out upon.
+ *
+ * @param recipients        The intended recipients of the message, may be null. See note above.
+ * @param channel           The channel the message is sent out upon.
  * @param serializedMessage The serialized message in MiniMessage form.
+ * @apiNote The {@code recipients} field may be null, indicating the message should be broadcast out on the channel, rather than sent only to the recipients.
  */
 @SuppressWarnings("unused")
-public record ChatMessage (@Nullable List<UUID> recipients, ChatChannel channel, String serializedMessage) {
+public record ChatMessage(@Nullable List<UUID> recipients, ChatChannel channel, String serializedMessage) {
 
     /**
      * A GSON object that will serialize nulls
@@ -27,9 +26,10 @@ public record ChatMessage (@Nullable List<UUID> recipients, ChatChannel channel,
 
     /**
      * Converts a string representation of this object to a new instance.
+     *
      * @param json The string representation.
-     * @see ChatMessage#toJson()
      * @return The {@link ChatMessage} object that results from the string.
+     * @see ChatMessage#toJson()
      */
     public static ChatMessage fromJson(String json) {
         return GSON.fromJson(json, ChatMessage.class);
@@ -37,8 +37,9 @@ public record ChatMessage (@Nullable List<UUID> recipients, ChatChannel channel,
 
     /**
      * Converts the object to a string representation of itself.
-     * @see ChatMessage#fromJson(String)
+     *
      * @return the string representation of this chat message.
+     * @see ChatMessage#fromJson(String)
      */
     public String toJson() {
         return GSON.toJson(this);
@@ -46,8 +47,9 @@ public record ChatMessage (@Nullable List<UUID> recipients, ChatChannel channel,
 
     /**
      * A method to stringify this object.
-     * @see ChatMessage#toJson()
+     *
      * @return the string representation of this object
+     * @see ChatMessage#toJson()
      */
     @Override
     public String toString() {
