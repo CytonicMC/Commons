5f967315299c287da518c084b1306f9a80536d82
foxikle <tristantroop260@gmail.com>
2024-08-11 09:51:25 -0400

    jd

--- src/main/java/net/cytonic/objects/TypedNamespace.java
+++ src/main/java/net/cytonic/objects/TypedNamespace.java
@@ -1,18 +1,14 @@
 package net.cytonic.objects;
 
-import com.google.gson.TypeAdapter;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
 import net.minestom.server.utils.NamespaceID;
-
-import java.io.IOException;
 
 
 /**
  * A record class holding a namespace and a type, to keep type safety
+ *
  * @param namespaceID the namespace
- * @param type the type of the preference
- * @param <T> the type of the preference
+ * @param type        the type of the preference
+ * @param <T>         the type of the preference
  */
 public record TypedNamespace<T>(NamespaceID namespaceID, Class<T> type) {
     // records are cool
